b316bf1e94e589495bdf02c0eb08b94e57e8b29c
Mahmoud Mesalem <mahmoudmesalem23@gmail.com>
2024-04-27 21:58:44 +0300

    Task 2 Done

--- Script/main.py
+++ Script/main.py
@@ -15,5 +15,31 @@ output = r'G:\Semster 8\GIS\Project\Output'
 feature_list = arcpy.ListFeatureClasses()
 print(feature_list)
 
-# Task 2
 
+# Task 2
+# Make feature layers
+arcpy.MakeFeatureLayer_management(land, "land_lyr")
+arcpy.MakeFeatureLayer_management(geography_points, "geo_points_lyr")
+arcpy.MakeFeatureLayer_management(elevation_points, "elevation_points_lyr")
+
+# Select elevation points on land
+arcpy.SelectLayerByLocation_management("elevation_points_lyr", "INTERSECT", "land_lyr")
+
+# Create shapefile for elevation points on land
+arcpy.FeatureClassToFeatureClass_conversion("elevation_points_lyr", output, "elevation_points_on_land.shp")
+
+# Count number of elevation points on land
+elevation_points_count = arcpy.GetCount_management("elevation_points_lyr")
+print("Number of elevation points on land:", elevation_points_count)
+
+# Select geography region points on land
+arcpy.SelectLayerByLocation_management("geo_points_lyr", "INTERSECT", "land_lyr")
+
+# Create shapefile for geography region points on land
+arcpy.FeatureClassToFeatureClass_conversion("geo_points_lyr", output, "geo_points_on_land.shp")
+
+# Count number of geography region points on land
+geo_points_count = arcpy.GetCount_management("geo_points_lyr")
+print("Number of geography region points on land:", geo_points_count)
+
+# Task 3
